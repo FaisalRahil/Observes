ca60e071d8e2b7baf59aa3e0113f9fa10cbb80c7
FaisalRahil <faisallrahil@gmail.com>
2015-04-08 10:44:26 +0200

    edit

--- routes/cpanel.js
+++ routes/cpanel.js
@@ -4,4 +4,19 @@ var router = express.Router();
 var userMgr = require('../app/user').userMgr;
 
 
+/* GET cpanel page. */
+router.get('/', function(req, res) {
+    res.render('cpanel',{title:'cpanel'});
+});
+
+/* GET cpanel page. */
+router.get('/cpanelAdmin', function(req, res) {
+    res.render('cpanelAdmin',{title:'cpanelAdmin'});
+});
+
+/* GET cpanel page. */
+router.get('/cpanelManager', function(req, res) {
+    res.render('cpanelManager',{title:'cpanelManager'});
+});
+
 module.exports = router;
