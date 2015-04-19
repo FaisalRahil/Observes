6ebfd7e903b16f6517f3a4af980e6c7ed5f3d1f9
abdooageel <ageel.abdoo@gmail.com>
2015-04-19 12:33:31 +0200

    add tabel

--- app/obs.js
+++ app/obs.js
@@ -1,6 +1,18 @@
 var mysqlMgr = require('./mysql').mysqlMgr,
   util=require('util');
-exports.officeMgr = {
+exports.obsMgr = {
+  getOrg : function(cb){
+    mysqlMgr.connect(function (conn) {
+      conn.query('SELECT * FROM `organisaition` WHERE `status`= 1 ',  function(err, result) {
+        conn.release();
+        if(err) {
+          util.log(err);
+        } else {
+          cb(result);
+        }
+      });
+    });
+  }
 
-	
+
 };
