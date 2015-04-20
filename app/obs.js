70bb2d3f59233c4436e2e4bb06e72b482d6cb4c8
FaisalRahil <faisallrahil@gmail.com>
2015-04-20 13:01:02 +0200

    edit

--- app/obs.js
+++ app/obs.js
@@ -1,18 +1,5 @@
 var mysqlMgr = require('./mysql').mysqlMgr,
   util=require('util');
 exports.obsMgr = {
-  getOrg : function(cb){
-    mysqlMgr.connect(function (conn) {
-      conn.query('SELECT * FROM `organisaition` WHERE `status`= 1 ',  function(err, result) {
-        conn.release();
-        if(err) {
-          util.log(err);
-        } else {
-          cb(result);
-        }
-      });
-    });
-  }
-
-
+  
 };
--- app/org.js
+++ app/org.js
@@ -1,6 +1,16 @@
 var mysqlMgr = require('./mysql').mysqlMgr,
   util=require('util');
-exports.officeMgr = {
-
-	
+exports.orgMgr = {
+	getOrg : function(cb){
+    mysqlMgr.connect(function (conn) {
+      conn.query('SELECT * FROM `organisaition` WHERE `status`= 1 ',  function(err, result) {
+        conn.release();
+        if(err) {
+          util.log(err);
+        } else {
+          cb(result);
+        }
+      });
+    });
+  }
 };
