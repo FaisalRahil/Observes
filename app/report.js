686d8651c92cc10a22c2e4bb4a565d01afc27362
FaisalRahil <faisallrahil@gmail.com>
2015-09-13 12:49:28 +0200

    edit

--- app/report.js
+++ app/report.js
@@ -1,6 +1,16 @@
 var mysqlMgr = require('./mysql').mysqlMgr,
   util=require('util');
-exports.officeMgr = {
-
-	
+exports.reportMgr = {
+  getAllObsAndOrg : function(cb){
+    mysqlMgr.connect(function (conn) {
+      conn.query('SELECT *,`org`.`email` AS email_org ,`obs`.`email` AS email_obs FROM `organisaition` org, `observers` obs WHERE `org`.`id_org` = `obs`.`registration_org`;',  function(err, result) {
+        conn.release();
+        if(err) {
+          util.log(err);
+        } else {
+          cb(result);
+        }
+      });
+    });
+  },
 };
--- routes/reports.js
+++ routes/reports.js
@@ -2,29 +2,72 @@ var express = require('express');
 var router = express.Router();
 var obsMgr = require('../app/obs').obsMgr;
 var orgMgr = require('../app/org').orgMgr;
+var reportMgr = require('../app/report').reportMgr;
 var app = require('express')();
 var jsr = require("jsreport");
 var fs = require("fs");
 var path = require("path");
+var nationality = require('../Nationality');
 
   router.get('/', function(req, res) {
 
   });
 
+  function drawAllResults(allResults){
+    var html = '';
+    var gender1;
+    var nat;
+    // for (i in allResults){
+    //   for (var i = 0; i <= nationality.length; i++) {
+    //     if( allResults[i].nationality == nationality ){
+
+    //     }
+    //   };
+      if(allResults[i].gender == 0){
+        gender1 = "ذكـر";
+      }else{
+        gender1 = "أنـثـى";
+      }
+      html+= '<tr>\
+                <td style="background-color:#FFFFC2 !important;"> '+allResults[i].name+' </td>\
+                <td style="background-color:#FFFFC2 !important;"> '+allResults[i].nationality+' </td>\
+                <td style="background-color:#FFFFC2 !important;"> '+allResults[i].pass_nid+' </td>\
+                <td style="background-color:#FFFFC2 !important;"> '+gender1+' </td>\
+                <td style="background-color:#FFFFC2 !important;"> '+allResults[i].email_obs+' </td>\
+                <td style="background-color:#FFFFC2 !important;"> '+allResults[i].phone_obs+' </td>\
+                <td style="background-color:#FFFFC2 !important;"> '+allResults[i].id_office+' </td>\
+                <td style="background-color:#FFFFC2 !important;"> '+allResults[i].registration_no+' </td>\
+                <td style="background-color:#FFFFC2 !important;"> '+allResults[i].name_org+' </td>\
+                <td style="background-color:#FFFFC2 !important;"> '+allResults[i].name_director+' </td>\
+                <td style="background-color:#FFFFC2 !important;"> '+allResults[i].type+' </td>\
+                <td style="background-color:#FFFFC2 !important;"> '+allResults[i].phone+' </td>\
+                <td style="background-color:#FFFFC2 !important;"> '+allResults[i].email_org+' </td>\
+                <td style="background-color:#FFFFC2 !important;"> '+allResults[i].address+' </td>\
+              </tr>';  
+    }
+    
+return html;
+  }
+
   router.get('/observers', function(req, res, next) {
-    jsr.render({
-      template: { 
-        content:  fs.readFileSync(path.join(__dirname, "../views/reports/observers.html"), "utf8"),
-        phantom: {
-          format: 'A3',
-          orientation: "landscape",
+    reportMgr.getAllObsAndOrg(function(results){
+      console.log(results);
+      jsr.render({
+        template: { 
+          content:  fs.readFileSync(path.join(__dirname, "../views/reports/observers.html"), "utf8"),
+          phantom: {
+            format: 'A3',
+            orientation: "landscape",
+          },
+          recipe: "phantom-pdf",
+          helpers:drawAllResults.toString()
         },
-        recipe: "phantom-pdf"
-      },
-      // data:obb
-    }).then(function (response) {
-      response.result.pipe(res);
-    });
+        data:{allResults:results}
+        // data:{:,:,dept:department,dev:devision,sys:system,obj:arabicTranscriptObject,ob:subj,o:array}
+      }).then(function (response) {
+        response.result.pipe(res);
+      });
+    }); 
   });
 
   // this obsByType // widght A4
@@ -77,18 +120,21 @@ var path = require("path");
 
   // this noOfLocaleObsEn // widght A4
   router.get('/noOfLocaleObsEn', function(req, res, next) {
-    jsr.render({
-      template: { 
-        content:  fs.readFileSync(path.join(__dirname, "../views/reports/noOfLocaleObsEn.html"), "utf8"),
-        phantom:{
-          orientation: "landscape",
+    reportMgr.getAllObsAndOrg(function(results){
+      console.log(results);
+      jsr.render({
+        template: { 
+          content:  fs.readFileSync(path.join(__dirname, "../views/reports/noOfLocaleObsEn.html"), "utf8"),
+          phantom:{
+            orientation: "landscape",
+          },
+          recipe: "phantom-pdf",
         },
-        recipe: "phantom-pdf",
-      },
-      // data:obj
-    }).then(function (response) {
-      response.result.pipe(res);
-    });
+        data:results
+      }).then(function (response) {
+        response.result.pipe(res);
+      });
+    }); 
   });
 
     // this noOfInternationalObs // widght A4
