7c1c3b4cc53d6e0acce1b780607741281d12c340
abdelqudoss <doss.aburass@gmail.com>
2015-10-15 10:20:42 +0200

    repo

--- app/report.js
+++ app/report.js
@@ -30,5 +30,32 @@ exports.reportMgr = {
       });
     });
   },
-  
+
+  noOfWomenAndMen : function(cb){
+    mysqlMgr.connect(function (conn) {
+      conn.query('SELECT COUNT( * ) AS man FROM  `observers` obs WHERE  `obs`.`gender` =1 LIMIT 0 , 30; SELECT COUNT( * ) AS woman FROM  `observers` obs WHERE  `obs`.`gender` =2 LIMIT 0 , 30;',function(err, result) {
+        console.log(result[0][0].man);
+          conn.release();
+          if(err) {
+            util.log(err);
+          } else {
+            cb(result);
+          }
+      });
+    });
+  },
+  obsByNationality : function(nat,cb){
+    mysqlMgr.connect(function (conn) {
+      conn.query('SELECT  `observers`.`name`, `observers`.`nationality`,`observers`.`registration_org`, `observers`.`status` , `organisaition`.`id_org` , `organisaition`.`type` FROM `observers`  LEFT JOIN  `organisaition` ON `observers`.`registration_org`=`organisaition`.`id_org` WHERE  `observers`.`status` = 1 && `observers`.`nationality`=?',nat,function(err, result) {
+        console.log("is");
+        console.log(result[0]);
+          conn.release();
+          if(err) {
+            util.log(err);
+          } else {
+            cb(result);
+          }
+      });
+    });
+  },
 };
